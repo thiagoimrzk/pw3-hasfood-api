2041d13ca2be71248a73736bd327d9432765f78f
thiagoimrzk <thiagoimrzk@gmail.com>
2022-03-28 07:56:27 -0300

    Criacao do Projeto

---
+++ pw3-hasfood-api/src/test/java/br/com/thiago/murozaki/pw3hasfoodapi/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiago.murozaki.pw3hasfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
